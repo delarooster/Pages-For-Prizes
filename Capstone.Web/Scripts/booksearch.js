f1218efc85aff11fc8761dcd044aeea97ddea6f7
Austin DeLaRosa <austin.delarosa@gmail.com>
2018-09-17 12:12:20 -0400

    Add Book API
    Add Book feature now pings Google API and returns JSON data which fills in the required Author and ISBN fields after a Title is provided

---
+++ Capstone.Web/Scripts/booksearch.js
@@ -0,0 +1,35 @@
+﻿function bookSearch() {
+    let title = document.getElementById('Title').value;
+    //let author = document.getElementById('Author').value;
+    document.getElementById('ISBN').innerHTML = "";
+    //console.log(title);
+    //console.log(author);
+
+    $.ajax({
+        url: "https://www.googleapis.com/books/v1/volumes?q=" + title + "+inauthor:" + author + "&maxResults=1",
+        dataType: "json",
+
+        success: function (data) {
+            let author = data.items[0].volumeInfo.authors[0];
+            let ISBN = data.items[0].volumeInfo.industryIdentifiers[0].identifier;
+            //passing JSON ISBN data to ISBN field
+            displayISBN(ISBN);
+            //passing JSON author data to Author field
+            displayAuthor(author);
+        },
+
+        type: 'GET'
+
+    });
+}
+function displayISBN(num) {
+    console.log(num);
+    $("#ISBN").val(num);
+}
+function displayAuthor(name) {
+    console.log(name);
+    $("#Author").val(name);
+}
+
+
+document.getElementById('Title').addEventListener("blur", bookSearch, false);
